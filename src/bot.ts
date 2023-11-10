69539b1dc8790680b422582b249a52339f9a673c
enoch-danijel <enochdanijel@gmail.com>
2023-11-10 09:24:55 +0100

    trying somethin new

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,7 @@
-import {
-	Bot,
-	Context,
-	InlineKeyboard,
-	InlineQueryResultBuilder,
-	session,
-} from "grammy";
+import { Bot, Context, session } from "grammy";
 import { commandsComposer } from "../commands";
 import { predictMarkeCapConversation } from "../conversations";
+import { conversations } from "../deno.dependencies";
 const { BOT_TOKEN: token = "" } = process.env;
 
 type MyContext = Context;
@@ -29,35 +24,6 @@ bot.api.setMyCommands([
 	},
 ]);
 
-bot.inlineQuery("/test", async (ctx) => {
-	// Create a single inline query result.
-	const result = InlineQueryResultBuilder.article(
-		"id:grammy-website",
-		"grammY",
-		{
-			reply_markup: new InlineKeyboard().url(
-				"grammY website",
-				"https://grammy.dev/"
-			),
-		}
-	).text(
-		`<b>grammY</b> is the best way to create your own Telegram bots.
-  They even have a pretty website! 👇`,
-		{ parse_mode: "HTML" }
-	);
-
-	// Answer the inline query.
-	await ctx.answerInlineQuery(
-		[result], // answer with result list
-		{ cache_time: 30 * 24 * 3600 } // 30 days in seconds
-	);
-});
-
-// Return empty result list for other queries.
-bot.on("inline_query", (ctx) => {
-	console.log(1111);
-	ctx.reply("inline Working");
-	ctx.answerInlineQuery([]);
-});
+bot.use(conversations());
 
 bot.use(commandsComposer);
