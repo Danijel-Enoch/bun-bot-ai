055e1785d7b47043ff0292a49db1400a6dd869f1
enoch-danijel <enochdanijel@gmail.com>
2023-11-10 09:27:42 +0100

    updated

--- deno.dependencies.ts
+++ deno.dependencies.ts
@@ -1,15 +1,6 @@
 export {
-	Bot,
-	Composer,
-	type Context,
-	InlineKeyboard,
-	session,
-	type SessionFlavor,
-	webhookCallback,
-} from "https://deno.land/x/grammy@v1.13.0/mod.ts";
-export {
 	type Conversation,
 	type ConversationFlavor,
 	conversations,
 	createConversation,
-} from "https://deno.land/x/grammy_conversations@v1.1.0/mod.ts";
+} from "@grammyjs/conversations";
